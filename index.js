91330fbf13cc1d1236130869dd0486f152227f21
Rogerio Lucio <rogeriob.lucio@hotmail.com>
2020-02-26 23:44:03 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const db = require('./config/db')
+const consign = require('consign')
+
+consign()
+.then('./config/middlewares.js')
+.then('./api')
+.then('./config/routes.js')
+.into(app)
+
+app.db = db
+
+app.get('/', (req, res) => {
+    res.status(200).send('Meu Backend Agora!')
+})
+
+app.listen(3000, () => {
+    console.log('BackEnd Executando...')
+})
